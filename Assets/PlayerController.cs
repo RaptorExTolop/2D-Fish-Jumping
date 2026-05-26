fdbcb830559fbebf80d8d9fa6d87e2e6b5dc26d8
RaptorExTolop <jamesappletonret@gmail.com>
2026-05-26 19:56:25 +0930

    We have a project :) started to set up the movement but it just feels horrible, going to have to change the movement to use rigidbody cause I am a dumba** :(

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,20 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using TreeEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
+using UnityEngine.XR;
 
 public class PlayerController : MonoBehaviour
 {
-    private Vector3 vel;
+    private float heldTime;
+    public float power = 20;
+    public float gravity = 200;
     // Start is called before the first frame update
     void Start() {
-        vel = Vector3.zero;
+        // vel = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update() {
+        if (Input.GetKey(KeyCode.Space)) {
+            heldTime += Time.deltaTime;
+        } else if (heldTime != 0) {
+            handleJump();
+        }
 
+        // vel.y = vel.y < 0 ? 0 : vel.y - gravity * Time.deltaTime;
+        // transform.position += vel;
+        transform.position = (new Vector3(0, Mathf.Clamp(transform.position.y, 0, 10), 0));
+    }
 
-        transform.position += vel;
+    void handleJump() {
+        heldTime = Mathf.Clamp(heldTime, 0.01f, 5);
+        float pow = heldTime * power;
+        Debug.Log("Jumping with power of " + pow);
+        // vel.y += pow;
+        heldTime = 0;
     }
 }
